b261595f413c2ec63a6db0c677d952cb06aeb532
rayenhdr93 <hedrirayen69@gmail.com>
2022-08-10 19:04:45 +0100

    8-

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -34,3 +34,10 @@ class Normal:
     def x_value(self, z):
         ''' Normalize Normal '''
         return ((z * self.stddev) + self.mean)
+
+    def pdf(self, x):
+        ''' Normalize Normal '''
+        π = 3.1415926536
+        e = 2.7182818285
+        p = (-0.5*((x - self.mean)/self.stddev)**2)
+        return ((1 / (self.stddev * ((2*π)**0.5))) * (e**p))
