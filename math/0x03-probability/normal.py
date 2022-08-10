c1362ced21ecbb915a7a94c93c951f9f3efd9df3
rayenhdr93 <hedrirayen69@gmail.com>
2022-08-10 18:45:18 +0100

    normal

---
+++ math/0x03-probability/normal.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+''' Normal calc '''
+
+
+class Normal:
+    ''' Normal calc '''
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        ''' Initialize Normal '''
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.stddev = float(stddev)
+            self.mean = float(mean)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            totdata = 0
+            for x in data:
+                totdata = totdata + x
+            self.mean = (totdata / len(data))
+            stdsquare = 0
+            for x in data:
+                stdsquare = stdsquare + ((x - self.mean) ** 2)
+            stdsquare = stdsquare / len(data)
+            self.stddev = stdsquare ** 0.5
