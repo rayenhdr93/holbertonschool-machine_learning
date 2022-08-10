a685ae463aaf05bd8434cd5eae676fe8cc173362
rayenhdr93 <hedrirayen69@gmail.com>
2022-08-10 17:50:39 +0100

    Exponential

---
+++ math/0x03-probability/exponential.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+''' Exponential calc '''
+
+
+class Exponential:
+    ''' Exponential calc '''
+
+    def __init__(self, data=None, lambtha=1.):
+        ''' Initialize Exponential '''
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            totdata = 0
+            for x in data:
+                totdata = totdata + x
+            self.lambtha = totdata / len(data)
