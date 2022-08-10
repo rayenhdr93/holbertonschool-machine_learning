ce9326f53374644bd43fac55a2ea329b97b7e15b
rayenhdr93 <hedrirayen69@gmail.com>
2022-08-10 17:04:15 +0100

    1-

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -18,3 +18,15 @@ class Poisson:
             for x in data:
                 totdata = totdata + x
             self.lambtha = totdata / len(data)
+
+    def pmf(self, k):
+        e = 2.7182818285
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return(0)
+        fac = 1
+        for i in range(k, 0, -1):
+            fac = fac * i
+        pmf = (e ** (-self.lambtha) * self.lambtha ** k) / fac
+        return(pmf)
