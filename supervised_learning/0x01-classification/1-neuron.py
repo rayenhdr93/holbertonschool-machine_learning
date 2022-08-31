65274b4c1be9b1fdaf157a01fba306a8cb4800ca
rayenhdr93 <hedrirayen69@gmail.com>
2022-08-31 17:46:02 +0100

    1-

---
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+'''Neuron is here'''
+
+
+import numpy as np
+
+
+class Neuron:
+    '''Neuron is here'''
+    def __init__(self, nx):
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
