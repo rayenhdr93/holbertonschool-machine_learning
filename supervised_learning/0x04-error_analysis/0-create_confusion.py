72a130011ccb0d1fa9aba6b87ed3e1c8dda127e9
rayenhdr93 <hedrirayen69@gmail.com>
2022-09-06 07:45:05 +0100

    fix

--- supervised_learning/0x04-error_analysis/0-create_confusion.py
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-''' Error Analysis '''
+''' Error Analysis is here'''
 
 
 import numpy
 
 
 def create_confusion_matrix(labels, logits):
-    ''' Error Analysis '''
     return (numpy.matmul(labels.T, logits))
